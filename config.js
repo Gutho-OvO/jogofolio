c7146b94dc3496e7eee9e0d572ae8edeb6d2127f
Gutho-OvO <thongustavo@gmail.com>
2025-12-27 13:27:07 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-s// ===== CONFIGURAÇÕES GLOBAIS =====
+// ===== CONFIGURAÇÕES GLOBAIS =====
 
 // Detecta se é mobile
 window.isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
