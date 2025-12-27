c0977e5794ece846a3cb09c97172b3c332c5f382
Gutho-OvO <thongustavo@gmail.com>
2025-12-27 21:58:33 +0000

    player_computador

--- player.js
+++ player.js
@@ -4,7 +4,7 @@ const player = {
     y: 0,
     width: 32,
     height: 32,
-    speed: 1.7, // Velocidade base - será ajustada dinamicamente
+    speed: 1.8, // Velocidade base - será ajustada dinamicamente
 
 
     direction: "down",
